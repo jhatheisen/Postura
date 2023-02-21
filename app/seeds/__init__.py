b4f49b52a385d64afa1bc5857414da798acf69d4
Jhass Theisen <jhasstheisen1@gmail.com>
2023-02-21 08:50:48 -0800

    Seed data

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .projects import seed_projects, undo_projects
+from .tasks import seed_tasks, undo_tasks
 
 from app.models.db import db, environment, SCHEMA
 
@@ -12,17 +14,23 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
-    seed_users()
+        undo_projects()
+        undo_tasks()
     # Add other seed functions here
+    seed_users()
+    seed_projects()
+    seed_tasks()
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
     # Add other undo functions here
+    undo_users()
+    undo_projects()
+    undo_tasks()
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,18 +1,26 @@
 from app.models import db, User, environment, SCHEMA
 
+# users
+
+demo = User(
+    username='Demo', email='demo@aa.io', password='password')
+marnie = User(
+    username='marnie', email='marnie@aa.io', password='password')
+bobbie = User(
+    username='bobbie', email='bobbie@aa.io', password='password')
+donnie = User(
+    username='donnie', email='donnie@aa.io', password='password')
+lena = User(
+    username='lena', email='lena@aa.io', password='password')
+
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
-
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
+    db.session.add(donnie)
+    db.session.add(lena)
     db.session.commit()
 
 
@@ -27,5 +35,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute("DELETE FROM users")
-        
+
     db.session.commit()
