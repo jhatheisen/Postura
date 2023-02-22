3a407862b4ccc5052762e37fbb58b1034e7af309
Jhass Theisen <jhasstheisen1@gmail.com>
2023-02-21 18:05:47 -0800

    Finished create project

---
+++ react-app/src/components/CreateProjectFormModal/index.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useModal } from "../../context/Modal";
+import { useHistory } from "react-router-dom";
+import { thunkCreateProject } from "../../store/project";
+import "./CreateProjectForm.css"
+
+function CreateProjectFormModal() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [errors, setErrors] = useState([]);
+  const { closeModal } = useModal();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // get date
+    const firstDash = dueDate.indexOf('-')
+    const year = dueDate.slice(0, firstDash)
+    const secondDash = dueDate.indexOf('-', firstDash + 1);
+    const month = dueDate.slice(firstDash + 1, secondDash);
+    const day = dueDate.slice(secondDash + 1)
+
+    const newProject = {}
+    if (name) newProject.name = name;
+    if (description) newProject.description = description;
+    if (dueDate) newProject.due_date = `${month}-${day}-${year}`;
+
+    console.log('new Project:', newProject)
+
+    const data = await dispatch(thunkCreateProject(newProject));
+
+    console.log('data:', data)
+
+    if (data.errors) {
+      setErrors(data.errors);
+    } else {
+        closeModal()
+    }
+  };
+
+  return (
+    <>
+      <h1>Create Project</h1>
+      <form onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <label>
+          Name
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Description
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+        <label>
+          Due Date
+          <input
+            type="date"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default CreateProjectFormModal;
--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,9 +1,68 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { thunkGetUserProjects } from '../../store/project';
+import CreateProjectFormModal from '../CreateProjectFormModal';
+import OpenModalButton from '../OpenModalButton';
 import './HomePage.css'
 
 function HomePage() {
+
+  const dispatch = useDispatch();
+  const ulRef = useRef();
+
+  const userProjects = useSelector(state => state.projects);
+  const user = useSelector(state => state.session.user);
+  const [showMenu, setShowMenu] = useState(false);
+
+  // const openMenu = () => {
+  //   if (showMenu) return;
+  //   setShowMenu(true);
+  // };
+
+  useEffect(() => {
+    dispatch(thunkGetUserProjects());
+
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  },[showMenu, dispatch])
+
+  const closeMenu = () => setShowMenu(false);
+
+  if (!userProjects) return null;
+
+  const today = new Date()
+  const dayWeek = today.toLocaleString('en-us', {  weekday: 'long' });
+  const month = today.toLocaleString('en-us', {  month: 'long' });
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className='homePage'>
+      <h2>{dayWeek}, {month} {today.getUTCDate()}</h2>
+      <h1>Hello, {user.username}</h1>
+      <div className='allProjectsBox'>
+        <OpenModalButton
+          buttonText="Create Project"
+          onItemClick={closeMenu}
+          className="CreateProjectButton"
+          modalComponent={<CreateProjectFormModal/>}
+        />
+        {
+          userProjects.map(project => (
+            <div className='projectBox'>
+              <NavLink exact to={`/projects/${project.id}`}>{project.name}</NavLink>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
